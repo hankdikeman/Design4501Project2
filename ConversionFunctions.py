500caef948895e41f5aaa77e82dd9dde9f3e385e
hankdikeman <dikem003@umn.edu>
2021-03-07 16:29:40 -0600

    conversion function template for reactors

---
+++ ConversionFunctions.py
@@ -0,0 +1,17 @@
+"""
+This file holds the implementation of reactor conversion files for the
+formaldehyde reactor, OME reactor, and methanol reactor for the equilibration
+function to determine steady state solution of reactor network.
+"""
+
+
+def MethanolReactor(inlets, temperature, pressure):
+    pass
+
+
+def FormaldehydeReactor(inlets, temperature, pressure):
+    pass
+
+
+def OMEReactor(inlets, temperature, pressure):
+    pass
